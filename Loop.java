3bbdd4e8aab5ec86dba784e5c65e8b24a59c1f54
Hacktoberfset-22 <gtyr5278@gmail.com>
2022-12-26 12:35:32 +0530

    Added Loops in Java

---
+++ Loop.java
@@ -0,0 +1,24 @@
+
+public class Loop {
+ public static void main(String[] args) {
+    //for loop
+    // for (int i = 0; i <24; i++) {
+    //     System.out.print(i+" ");
+        
+    // }
+//while loop
+int i=0;
+// while (i<10) {
+//     System.out.println(i);
+//     i++;
+do {
+    System.out.println(i);
+    i++;
+    
+} while (i<10);
+
+    
+}
+
+ }
+    
