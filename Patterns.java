3c81687eb6014260d9d3811caa0685b5657c6f8f
Hacktoberfset-22 <gtyr5278@gmail.com>
2022-12-26 20:44:45 +0530

    Added some more patterns problems

--- Patterns.java
+++ Patterns.java
@@ -57,17 +57,61 @@ public class Patterns {
         // System.out.println();
         // }
         // Question -6 Half Pyramid 180 degree
-        for (int i = 1; i <= n; i++) {
-            // for spaces
-            for (int j = 1; j <=n - i; j++) {
-                System.out.print(" ");
-            }
-            // for stars
-            for (int j = 1; j <=i; j++) {
-                System.out.print("*");
+        // for (int i = 1; i <= n; i++) {
+        //     // for spaces
+        //     for (int j = 1; j <=n - i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     // for stars
+        //     for (int j = 1; j <=i; j++) {
+        //         System.out.print("*");
 
+        //     }
+        //     System.out.println();
+        // }
+        // Question -7 Half Pyramid With Numbers
+        // for (int i = 1; i <=n; i++) {
+        //     for (int j = 1; j <=i; j++) {
+        //         System.out.print(j+" ");
+                
+        //     }
+        //     System.out.println();
+        // }
+        //Question-8 Inverted pyramid with Numbers 
+        // for (int i = n; i>=1; i--) {
+        //     for (int j = 1; j <=i; j++) {
+        //         System.out.print(j+" ");
+                
+        //     }
+        //     System.out.println();
+            
+        // }
+        // Question -9 Floyd's Traingle
+        // int numbers=1;
+        // for (int i = 1; i<=n; i++) {
+        //     for (int j = 1; j <=i; j++) {
+        //         System.out.print(numbers+" ");
+        //         numbers++;
+                
+        //     }
+        //     System.out.println();
+            
+        // }
+        //Question -10 0-1 Triangle
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=i; j++) {
+                int sum=i+j;
+                if (sum%2==0) {
+                    System.out.print(1+" ");
+                }
+                else{
+                    System.out.print(0+" ");
+                }
+                
             }
             System.out.println();
+            
         }
+
     }
 }
