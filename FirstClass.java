4028b6b6b6cfa7c85dabd61b05b2cd8c0eb636fc
Hacktoberfset-22 <gtyr5278@gmail.com>
2022-12-26 10:45:34 +0530

    Added My First 2 programs

---
+++ FirstClass.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class FirstClass{
+    public static void main(String args[]) {
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+        Scanner sc = new Scanner(System.in);
+        final float b= 3.14f;
+        float a= sc.nextFloat();
+        float area = b*a*a;
+
+        // String name = sc.nextLine();
+        System.out.println(area);
+        
+        // variables 
+        // int a =5;
+        // int b=6;
+        // int sum = a+b;
+        // System.out.println(sum);
+
+    }}
