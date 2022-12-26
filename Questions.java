ec1bf837e0908515509cf671bf701551b6669e56
Hacktoberfset-22 <gtyr5278@gmail.com>
2022-12-26 13:01:34 +0530

    Added Some Questions in Java

---
+++ Questions.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Questions {
+public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter The Number ");
+  // Sum of Numbers From 1 to n
+//   int sum =0;
+//   int n  = sc.nextInt();
+//   for (int i = 1; i <=n; i++) {
+//     sum=sum+i;
+    
+//   }
+//   System.out.println("The sum is "+ sum);
+// Table of n;
+ int n = sc.nextInt();
+ System.out.println("The Table is :");
+ for (int i = 1; i <=10; i++) {
+    int tb=n*i;
+    System.out.println(n+"x"+i+"="+tb);
+    
+ }
+}
+    
+}
