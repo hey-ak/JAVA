e7a7b7157778ec95e6dc3d743c0430a6d19b9b06
Akshay <akshay0127.be21@chitkara.edu.in>
2022-12-30 17:12:53 +0530

    Added update bit manupulation file

--- BitManupulation.java
+++ BitManupulation.java
@@ -7,14 +7,26 @@ public class BitManupulation {
         int n=sc.nextInt();
         System.out.println("Enter Position");
         int pos= sc.nextInt();
+        // get bit
         int bitmask= 1<<pos;
-        if ((bitmask & n)==0) {
-            System.out.println("Bit Was Zero");
+        
+        // if ((bitmask & n)==0) {
+        //     System.out.println("Bit Was Zero");
             
-        }
-        else{
-            System.out.println("Bit Was One");
-        }
+        // }
+        // else{
+        //     System.out.println("Bit Was One");
+        // }
+        // set bit
+        // int newNumber= bitmask |n;
+        // System.out.println("The New Number is ");
+        // System.out.println(newNumber);
+        // clear bit
+        int notBitmask= ~(bitmask);
+        int newNumber= notBitmask & n;
+        System.out.print("The New Number is ");
+        System.out.println(newNumber);
+       
 
     }
     
