9fd399310cec82f43fb8b18a26625af1dd9842cf
Akshay <akshay0127.be21@chitkara.edu.in>
2022-12-30 11:42:28 +0530

    Added string Bulder.java

---
+++ Sb.java
@@ -0,0 +1,21 @@
+public class Sb {
+ public static void main(String[] args) {
+    StringBuilder sb= new StringBuilder("Tony");
+    // Display Char At 0
+    System.out.println(sb);
+    // set Index at zero
+    sb.setCharAt(0, 'P');
+    System.out.println(sb);
+    sb.insert(2,'n');
+    System.out.println(sb);
+    sb.delete(2,3);
+    System.out.println(sb);
+    sb.delete(2,4);
+    System.out.println(sb);
+
+
+
+
+ }
+    
+}
