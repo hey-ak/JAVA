eb7ba83b3a375f05b0ca9279f80b3d71002964f3
Akshay <akshay0127.be21@chitkara.edu.in>
2022-12-30 10:54:20 +0530

    Added Some More in Strings.java

--- Strings.java
+++ Strings.java
@@ -3,20 +3,31 @@ import java.util.Scanner;
 public class Strings {
     public static void main(String[] args) {
         //  concatenation
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Your First Name");
-        String Fname = sc.nextLine();
-        System.out.println("Enter Your Last Name");
-        String Lname = sc.nextLine();
-        String Fullname=Fname+" "+Lname;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Your First Name");
+        // String Fname = sc.nextLine();
+        // System.out.println("Enter Your Last Name");
+        // String Lname = sc.nextLine();
+        // String Fullname=Fname+" "+Lname;
 
-        System.out.println("Your Name is "+ Fullname);
-        System.out.println(Fullname.length());
-        // CharAt
-        for (int i = 0; i <Fullname.length(); i++) {
-            System.out.println(Fullname.charAt(i));
+        // System.out.println("Your Name is "+ Fullname);
+        // System.out.println(Fullname.length());
+        // // CharAt
+        // for (int i = 0; i <Fullname.length(); i++) {
+        //     System.out.println(Fullname.charAt(i));
             
-        }
+        // }
+        // // compare
+        // if (Fname.compareTo(Lname)==0) {
+        //     System.out.println("Strings Are Equal");
+            
+        // }else{
+        //     System.out.println("Strings Are Not Equal");
+        // }
+        // substring
+        String Sentence="Tony Stark";
+        String name=Sentence.substring(5,Sentence.length());
+        System.out.println(name);
 
     
 
