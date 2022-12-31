75a839bcd3213791f81473135bd7818ff6c5a437
Akshay <akshay0127.be21@chitkara.edu.in>
2022-12-31 13:15:28 +0530

    Added interfaces in java to implement abstraction

---
+++ Interfaces.java
@@ -0,0 +1,28 @@
+interface Animal {
+    void walk();
+ }
+  
+ class Horse implements Animal {
+    public void walk() {
+        System.out.println("Horse walks on 4 legs");
+    }
+ }
+  
+ class Chicken implements Animal {
+    public void walk() {
+        System.out.println("Chicken walks on 2 legs");
+    }
+ }
+  
+ public class Interfaces {
+    public static void main(String args[]) {
+       Horse horse = new Horse();
+       horse.walk();
+    }
+ }
+ 
+
+
+
+
+
